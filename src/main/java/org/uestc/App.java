ee4e58bb6cac1b7ffda47bc8fda9ed09d7a696d3
lijiuhong <397363163@qq.com>
2020-10-02 20:03:05 +0800

    initial commit

---
+++ src/main/java/org/uestc/App.java
@@ -0,0 +1,13 @@
+package org.uestc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/uestc/leetcode/simple/Simple_01.java
@@ -0,0 +1,17 @@
+package org.uestc.leetcode.simple;
+
+import org.junit.Test;
+
+public class Simple_01 {
+
+
+    public void func(String str) {
+        System.out.println(str);
+    }
+
+    @Test
+    public void codeTest() {
+        String str = "hello word!";
+        func(str);
+    }
+}
